5febf94652609981911f6bb54c8ca006bd30950a
Wyatt <95558468+KCNOSU29K3@users.noreply.github.com>
2022-02-08 14:45:26 -0700

    Fixed several bugs
    Program is now functioning; Vault operations are not yet implemented.

--- Revenant_install_files/Revenant.py
+++ Revenant_install_files/Revenant.py
@@ -111,7 +111,7 @@ class log:
                         string_ = "encrypted"
                     else:
                         string_ = "decrypted"
-                    file.write("Singular: File: {} was: {} at: {} UTC".format(path, string_, time_))
+                    file.write("Singular: File: {} was: {} at: {} UTC\n".format(path, string_, time_))
                 pass
             elif mode == "f":
                 with open("/home/{}/Revenant/userLog.txt".format(self.username) ,'a') as file:
@@ -181,7 +181,7 @@ class cipher:
         self.password = password
         self.username = username
         self.marker  = b"E1m%nj2i$bhilj"
-        self.logger = log
+        self.logger = log(self.username)
 
 
     def log(self, mode=str, path=str, encrypted=bool) -> int:
@@ -203,15 +203,15 @@ class cipher:
             6: File encryption marker detected during encryption.
             7: File encryption marker not detected during decryption.
         """
+        if path is None:
+            return 1
         # checking if file is hidden
         if "/." in path:
             return 4
         # checking if file is part of the root filesystem and not a part of ChromeOS for linux in Chromebooks
         if "/home/" not in path and "/mnt/chromeos/MyFiles" not in path:
             return 5
-        # checking if decrypt is cancelled
-        if path is None:
-            return 1
+        # checking if decrypt is cancelleD
         try:
             if os.path.isfile(path) is True:
                 with open (path, "rb") as file:
@@ -272,20 +272,20 @@ class cipher:
             objects = [return_code, salt, iv, key, key_hash, key_signature, data, cipher, ciphered_data]
             for object_ in objects:
                 del object_
-            self.log()
+            self.log("s", path, encrypted=True)
             return 0
         elif os.path.isdir(path) is True:
-            for root, dirs, files in os.walk():
+            for root, dirs, files in os.walk(path):
                 for file in files:
                     file_name = os.path.join(root, file)
                     return_code = self.guard_clause(file_name, decrypting=False)
                     if return_code != 0:
                         continue
-                    with open (file_name) as file:
+                    with open (file_name, "rb") as file:
                         data = file.read()
                     cipher = AES.new(key, AES.MODE_CFB, iv=iv)
                     ciphered_data = cipher.encrypt(data)
-                    with open(path, "wb") as file:
+                    with open(file_name, "wb") as file:
                         file.write(self.marker)
                         file.write(iv)
                         file.write(salt)
@@ -294,13 +294,13 @@ class cipher:
                     objects = [return_code, salt, iv, key, key_hash, key_signature, data, cipher, ciphered_data]
                     for object_ in objects:
                         del object_
-            self.log()
+            self.log("f", path, encrypted=True)
             return 0
         else:
             sg.popup_auto_close("WHAT DID YOU DO????? the app didn't detect the file as either a file or a dir. what? email me pls", font="Helvetica")    
 
 
-    def decrypt(self, filepath=str) -> int:
+    def decrypt(self, path=str) -> int:
         """
         A method for decrypting an encrypted file.
 
@@ -316,11 +316,11 @@ class cipher:
             6: File was encrypted with a different key.
             7: File was not encrypted.
         """
-        return_code = self.guard_clause(filepath, decrypting=False)
+        return_code = self.guard_clause(path, decrypting=True)
         if return_code != 0:
             return return_code
-        if os.path.isfile(filepath) is True:
-            with open (filepath, "rb") as file:
+        if os.path.isfile(path) is True:
+            with open (path, "rb") as file:
                 file.read(14)
                 iv = file.read(16)
                 salt = file.read(32)
@@ -335,15 +335,15 @@ class cipher:
                 return 6
             cipher = AES.new(key, AES.MODE_CFB, iv=iv)
             original_data = cipher.decrypt(ciphered_data)
-            with open(filepath, "wb") as file:
+            with open(path, "wb") as file:
                 file.write(original_data)
             objects = [return_code, iv, salt, file_signature, ciphered_data, key, key_hash, key_signature, cipher, original_data]
             for object_ in objects:
                 del object_
-            self.log()
+            self.log("s", path, encrypted=False)
             return 0
-        elif os.path.isdir(filepath) is True:
-            for root, dirs, files in os.walk(filepath):
+        elif os.path.isdir(path) is True:
+            for root, dirs, files in os.walk(path):
                 for file in files:
                     file_name = os.path.join(root, file)
                     return_code = self.guard_clause(file_name, decrypting=True)
@@ -366,7 +366,7 @@ class cipher:
                     objects = [return_code, iv, salt, file_signature, ciphered_data, key, key_hash, key_signature, cipher, original_data]
                     for object_ in objects:
                         del object_
-            self.log()
+            self.log("f", path, encrypted=False)
             return 0    
         else:
             sg.popup_auto_close("WHAT DID YOU DO????? the app didn't detect the file as either a file or a dir. what? email me pls", font="Helvetica")    
@@ -408,6 +408,7 @@ class cipher:
                         original_data = cipher.decrypt(ciphered_data)
                         with open (file_name, "wb") as file:
                             file.write(original_data)
+                self.log("v", path=" ", encrypted=False)
                 return 0
             except FileNotFoundError:
                 try:
@@ -440,6 +441,7 @@ class cipher:
                             file.write(salt)
                             file.write(key_signature)
                             file.write(ciphered_data)
+                self.log("v", path=" ", encrypted=True)
                 return 0                        
             except FileNotFoundError:
                 try:
@@ -507,6 +509,7 @@ class login:
         if password_hash == hex_digest:
             return 0
         elif password_hash != hex_digest:
+            sleep(5)
             return 2
 
 
@@ -528,9 +531,9 @@ class login:
             sg.popup_error("Password cache not found. Reinstall with the most recent password and decrypt any encrypted files. This program will now quit.", font='Helvetica')
             quit()
         # getting hash and salt
-        length = len(contents); pass_hash = contents[0:length:4]; salt = contents[1:length:4]
+        length = len(contents); pass_hash = contents[0:length:4]; salt1 = contents[1:length:4]
         # creating hash object and comparing
-        full = old_password + username.strip() + salt; hashed = hash.sha256(full.encode()); hex_string = hashed.hexdigest()
+        full = old_password + username.strip() + salt1; hashed = hash.sha256(full.encode()); hex_string = hashed.hexdigest()
         if hex_string == pass_hash:
             # creating new salt and two filler hash strings
             new_salt = self.rand_hash(); filler_1 = self.rand_hash(); filler_2 = self.rand_hash()
@@ -566,9 +569,7 @@ class login:
 
 
 sg.theme('DarkBlue')
-
-
-# Creating the login layout to get password + username from the user
+# Graphical layouts to simulate an interactive window
 login_layout = [
     [sg.Text('Enter current Linux username. This will be used to access relevant directories/files.', font="Helvetica")],
     [sg.InputText(key='username', font="Helvetica", border_width=10)],
@@ -576,16 +577,12 @@ login_layout = [
     [sg.InputText(key="password", password_char='*', font="Helvetica", border_width=10)],
     [sg.Button("Ok", font="Helvetica", border_width=10), sg.Button("Close Window", font="Helvetica", border_width=10)]
 ] # this layout is done
-
-
 logged_layout = [
     [sg.Text('Password confirmed, user logged. What would you like to do?', font="Helvetica", border_width=10)],
     [sg.Button('Edit file configurations', key="file_hub", font="Helvetica", border_width=10),
     sg.Button('Change my password', key='password_change', font="Helvetica", border_width=10)],
     [sg.Button('Close Window', key='Close', font="Helvetica", border_width=10)]
 ] # this layout is done
-
-
 change_pass_layout = [
     [sg.Text("Please enter your current password.", font="Helvetica")],
     [sg.InputText(key="old_password", password_char='*', font="Helvetica", border_width=10)],
@@ -593,8 +590,6 @@ change_pass_layout = [
     [sg.InputText(key="new_password", password_char="*", font="Helvetica", border_width=10)],
     [sg.Button("Ok", key="OK_pass", font="Helvetica", border_width=10), sg.Button("Go back", key="back_pass", font="Helvetica", border_width=10)]
 ] # also done
-
-
 file_hub_script_layout = [
     [sg.Text("Please select a command.", font="Helvetica")],
     [sg.Frame("External file commands",[
@@ -612,8 +607,6 @@ file_hub_script_layout = [
     [sg.Button('Back - Go back to previous screen', key='back', font="Helvetica", border_width=10), sg.Button("Audit UserLog File", key="Intiate_audit", font="Helvetica", border_width=10)],
     [sg.Button("Logout - terminates application and logs user out.", key="logout", font="Helvetica", border_width=10)]
 ]# this layout is done
-
-
 userLog_audit_layout = [
     [sg.Text("UserLog Audit Mode selected. What would you like to do?")],
     [sg.Button("Remove singular file Encryption/Decryption logs", key="single_file_log_audit", font="Helvetica", border_width=10),
@@ -623,8 +616,6 @@ userLog_audit_layout = [
     [sg.Button("Return to previous screen", key="audit_return", font="Helvetica", border_width=10),
     sg.Button("Close Window", key="Close_Audit", font="Helvetica", border_width=10)]
 ]
-
-
 layout = [
     [sg.Column(login_layout, key="Login_Layout", element_justification="C"),
     sg.Column(logged_layout, visible=False, key="Logged_Layout", element_justification="C"),
@@ -648,14 +639,14 @@ class Main:
     def __init__(self) -> None:
         pass
 
-    def intialize():
+    def intialize(self):
         """
         Intializes the app.
 
         Does not return; call on last line.
         """
         # setting needed variables
-        username = "0"; password = "0"; count = 0; LAYOUT_CYCLE_VAR = 0; logged_in = False; count = 0; alert = 0
+        username = "0"; password = "0"; count = 0; LAYOUT_CYCLE_VAR = 0; logged_in = False; count = 0; alert = 0; logger = 0; encrypter = 0
         # setting the login class
         login_class = login()
         while True:
@@ -674,7 +665,7 @@ class Main:
                     logged_in = True
                     encrypter = cipher(password=password, username=username)
                     logger = log(username=username)
-                    log.log_login()
+                    logger.log_login()
                 elif exit_code == 1:
                     sg.popup_auto_close("The given username is not a valid username.", font="Helvetica", non_blocking=True)
                 elif exit_code == 2:
@@ -709,78 +700,79 @@ class Main:
                 # getting file:
                 file = sg.popup_get_file("Select the file you wish to encrypt.", font="Helvetica")
                 exit_code = encrypter.encrypt(file)
-                if exit_code == "0":
+                if exit_code == 0:
                     sg.popup_auto_close("File encryption successful.", font="Helvetica", non_blocking=True)
-                elif exit_code == "1":
+                elif exit_code == 1:
                     sg.popup_auto_close("File encryption cancelled.", font="Helvetica", non_blocking=True)
-                elif exit_code == "2":
+                elif exit_code == 2:
                     sg.popup_auto_close("File path is invalid.", font="Helvetica", non_blocking=True)
-                elif exit_code == "3":
+                elif exit_code == 3:
                     sg.popup_auto_close("App does not have permission to access file.", font="Helvetica", non_blocking=True)
-                elif exit_code == "4":
+                elif exit_code == 4:
                     sg.popup_auto_close("File is hidden.", font="Helvetica", non_blocking=True)
-                elif exit_code == "5":
+                elif exit_code == 5:
                     sg.popup_auto_close("File is part of root filesystem and not part of ChromeOS.", font="Helvetica", non_blocking=True)
-                elif exit_code == "6":
+                elif exit_code == 6:
                     sg.popup_auto_close("File was already encrypted.", font="Helvetica", non_blocking=True)
             elif event == "file_decrypt":
-                file = sg.popup_get_file("Select the file you wish to decrypt.", font="Helvetica", non_blocking=True)
+                file = sg.popup_get_file("Select the file you wish to decrypt.", font="Helvetica")
                 exit_code = encrypter.decrypt(file)
-                if exit_code == "0":
+                if exit_code == 0:
                     sg.popup_auto_close("File decryption successful.", font="Helvetica", non_blocking=True)
-                if exit_code == "1":
+                if exit_code == 1:
                     sg.popup_auto_close("File decryption cancelled.", font="Helvetica", non_blocking=True)
-                if exit_code == "2":
+                if exit_code == 2:
                     sg.popup_auto_close("File path is invalid.", font="Helvetica", non_blocking=True)
-                if exit_code == "3":
+                if exit_code == 3:
                     sg.popup_auto_close("App does not have permission to access file.", font="Helvetica", non_blocking=True)
-                if exit_code == "4":
+                if exit_code == 4:
                     sg.popup_auto_close("File is hidden.", font="Helvetica", non_blocking=True)
-                if exit_code == "5":
+                if exit_code == 5:
                     sg.popup_auto_close("File is part of root filesystem and not part of ChromeOS.", font="Helvetica", non_blocking=True)
-                if exit_code == "6":
+                if exit_code == 6:
                     sg.popup_auto_close("File was encrypted with a different key.", font="Helvetica", non_blocking=True)
-                if exit_code == "7":
+                if exit_code == 7:
                     sg.popup_auto_close("File was not encrypted.", font="Helvetica", non_blocking=True)
             elif event == "folder_encrypt":
                 folder = sg.popup_get_folder("Select the file you wish to encrypt.", font="Helvetica")
                 exit_code = encrypter.encrypt(folder)
-                if exit_code == "0":
+                if exit_code == 0:
                     sg.popup_auto_close("Folder encryption successful.", font="Helvetica", non_blocking=True)
-                elif exit_code == "1":
+                elif exit_code == 1:
                     sg.popup_auto_close("Folder encryption cancelled.", font="Helvetica", non_blocking=True)
-                elif exit_code == "2":
+                elif exit_code == 2:
                     sg.popup_auto_close("Folder path is invalid.", font="Helvetica", non_blocking=True)
-                elif exit_code == "3":
+                elif exit_code == 3:
                     sg.popup_auto_close("App does not have permission to access file.", font="Helvetica", non_blocking=True)
-                elif exit_code == "4":
+                elif exit_code == 4:
                     sg.popup_auto_close("Folder is hidden.", font="Helvetica", non_blocking=True)
-                elif exit_code == "5":
+                elif exit_code == 5:
                     sg.popup_auto_close("Folder is part of root filesystem and not part of ChromeOS.", font="Helvetica", non_blocking=True)
-                elif exit_code == "6":
+                elif exit_code == 6:
                     sg.popup_auto_close("Folder was already encrypted.", font="Helvetica", non_blocking=True)
                 pass
             elif event == "folder_decrypt":
-                folder = sg.popup_get_folder("Select the file you wish to decrypt.", font="Helvetica", non_blocking=True)
+                folder = sg.popup_get_folder("Select the file you wish to decrypt.", font="Helvetica")
                 exit_code = encrypter.decrypt(folder)
-                if exit_code == "0":
+                if exit_code == 0:
                     sg.popup_auto_close("Folder decryption successful.", font="Helvetica", non_blocking=True)
-                if exit_code == "1":
+                if exit_code == 1:
                     sg.popup_auto_close("Folder decryption cancelled.", font="Helvetica", non_blocking=True)
-                if exit_code == "2":
+                if exit_code == 2:
                     sg.popup_auto_close("Folder path is invalid.", font="Helvetica", non_blocking=True)
-                if exit_code == "3":
+                if exit_code == 3:
                     sg.popup_auto_close("App does not have permission to access file.", font="Helvetica", non_blocking=True)
-                if exit_code == "4":
+                if exit_code == 4:
                     sg.popup_auto_close("Folder is hidden.", font="Helvetica", non_blocking=True)
-                if exit_code == "5":
+                if exit_code == 5:
                     sg.popup_auto_close("Folder is part of root filesystem and not part of ChromeOS.", font="Helvetica", non_blocking=True)
-                if exit_code == "6":
+                if exit_code == 6:
                     sg.popup_auto_close("Folder was encrypted with a different key.", font="Helvetica", non_blocking=True)
-                if exit_code == "7":
+                if exit_code == 7:
                     sg.popup_auto_close("Folder was not encrypted.", font="Helvetica", non_blocking=True)
                 pass
             elif event == "vault_encrypt":
+                sg.popup_error("501 - NOT YET IMPLEMENTED")
                 pass
             elif event == "OK_pass":
                 old_pass = values["old_password"]
@@ -807,5 +799,5 @@ class Main:
                 sg.popup_auto_close("Selected entries cleared.", font="Helvetica", non_blocking=True)
 
 
-app = Main
+app = Main()
 app.intialize()
